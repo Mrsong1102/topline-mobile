8b4feaba5003b5091283ec3f20227b194077463c
zhangyu <zhangyu@163.com>
2019-07-09 01:51:18 +0800

    登录-准备页面结构布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <p>Login Component</p>
+  <div class="login-wrap">
+    <van-nav-bar title="标题" />
+    <form>
+      <van-cell-group>
+        <van-field v-model="user.mobile"
+                   required
+                   clearable
+                   label="手机号"
+                   placeholder="请输入手机号" />
+
+        <van-field v-model="user.code"
+                   type="password"
+                   label="密码"
+                   placeholder="请输入密码"
+                   required />
+      </van-cell-group>
+      <div class="login-btn">
+        <van-button class="btn"
+                    type="info">登录</van-button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -9,11 +28,21 @@ export default {
   name: 'LoginIndex',
   data () {
     return {
-
+      user: {
+        mobile: '',
+        code: ''
+      }
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.login-btn {
+  padding: 20px;
+  .btn {
+    width: 100%;
+    border-radius: 8px;
+  }
+}
 </style>
