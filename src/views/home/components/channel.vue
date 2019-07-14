e11f3a028c0744c9cae3845fa3214c385b2acc15
zhangyu <zhangyu@163.com>
2019-07-14 23:03:17 +0800

    频道管理-点击显示频道管理组件

---
+++ src/views/home/components/channel.vue
@@ -0,0 +1,25 @@
+<template>
+  <van-popup :value="value"
+             @input="$emit('input',$event)"
+             position="bottom"
+             :style="{ height: '93%' }" />
+</template>
+
+<script>
+export default {
+  name: 'HomeChannel',
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,62 +1,71 @@
 <template>
-  <div>
-    <div class="home">
-      <van-nav-bar title="首页"
-                   fixed />
-      <!--
+  <div class="home">
+    <van-nav-bar title="首页"
+                 fixed />
+    <!--
         activeChannelIndex 绑定当前激活的标签页，使用索引
        -->
-      <van-tabs class="channel-tabs"
-                v-model="activeChannelIndex">
-        <div slot="nav-right"
-             class="wap-nav">
-          <van-icon name="wap-nav" />
-        </div>
-        <van-tab v-for="channelItem in channels"
-                 :key="channelItem.id"
-                 :title="channelItem.name">
-          <!--
+    <van-tabs class="channel-tabs"
+              v-model="activeChannelIndex">
+      <div slot="nav-right"
+           class="wap-nav"
+           @click="isChannelShow = true">
+        <van-icon name="wap-nav" />
+      </div>
+      <van-tab v-for="channelItem in channels"
+               :key="channelItem.id"
+               :title="channelItem.name">
+        <!--
             下拉刷新
             isLoading 用来控制下拉树新的loading状态，下拉刷新的时候，它会自动将loading设置为true
             @refresh当下拉刷新的时候会触发
            -->
-          <van-pull-refresh v-model="channelItem.downPullLoading"
-                            @refresh="onRefresh"
-                            :success-text="channelItem.downPullSuccessText"
-                            :success-duration="1000">
-            <!-- 列表组件：主要提供上拉加载更多的功能
+        <van-pull-refresh v-model="channelItem.downPullLoading"
+                          @refresh="onRefresh"
+                          :success-text="channelItem.downPullSuccessText"
+                          :success-duration="1000">
+          <!-- 列表组件：主要提供上拉加载更多的功能
               loading 用来控制是否加载完毕
               finished 用来控制是否加载完毕
               @load 加载更多的时候触发一个事件，它自动会调用onload函数拿数据，以填充页面，他每次调用onload会自动将loading设置为true，我们需要在onload中拿到本次加载的数据以后，将loading设置为false
            -->
-            <van-list v-model="channelItem.upPullLoading"
-                      :finished="channelItem.upPullFinished"
-                      finished-text="没有更多了"
-                      @load="onLoad">
-              <van-cell v-for="articleItem in channelItem.articles"
-                        :key="articleItem.art_id"
-                        :title="articleItem.title" />
-            </van-list>
-          </van-pull-refresh>
-        </van-tab>
-      </van-tabs>
-    </div>
+          <van-list v-model="channelItem.upPullLoading"
+                    :finished="channelItem.upPullFinished"
+                    finished-text="没有更多了"
+                    @load="onLoad">
+            <van-cell v-for="articleItem in channelItem.articles"
+                      :key="articleItem.art_id"
+                      :title="articleItem.title" />
+          </van-list>
+        </van-pull-refresh>
+      </van-tab>
+    </van-tabs>
+
+    <!-- 频道管理组件 -->
+    <!--
+      v-model 实际上是：
+       v-bind:value="数据"
+       v-on:input="数据=$event"
+     -->
+    <HomeChannel v-model="isChannelShow" />
+    <!-- 频道管理组件 -->
   </div>
 </template>
 
 <script>
 import { getUserChannels } from '@/api/channel'
 import { getArticles } from '@/api/article'
+import HomeChannel from './components/channel'
 export default {
   name: 'HomeIndex',
+  components: {
+    HomeChannel
+  },
   data () {
     return {
       activeChannelIndex: 0,
-      list: [],
-      loading: false,
-      finished: false,
-      isLoading: false,
-      channels: [] // 存储频道列表
+      channels: [], // 存储频道列表
+      isChannelShow: false
     }
   },
 
